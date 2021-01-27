ee56240985053e8e41a26271ed8dcded50bd919b
xhlulu <xinghan@plot.ly>
2021-01-26 20:11:45 -0500

    Re-apply black on usage demo

--- demos/usage-algorithm/app.py
+++ demos/usage-algorithm/app.py
@@ -8,59 +8,75 @@ from dash.dependencies import Input, Output, State
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
-vtk_view = dash_vtk.View([
-    dash_vtk.GeometryRepresentation([
-        dash_vtk.Algorithm(
-            id='vtk-algorithm',
-            vtkClass="vtkConeSource",
-            state={'capping': False, 'resolution': 60}
+vtk_view = dash_vtk.View(
+    [
+        dash_vtk.GeometryRepresentation(
+            [
+                dash_vtk.Algorithm(
+                    id="vtk-algorithm",
+                    vtkClass="vtkConeSource",
+                    state={"capping": False, "resolution": 60},
+                )
+            ]
         )
-    ])
-])
+    ]
+)
 
-controls = dbc.Card([
-    dbc.CardHeader("Controls"),
-    dbc.CardBody([
-        html.P("Resolution:"),
-        dcc.Slider(
-            id='slider-resolution', 
-            min=10, max=100, step=1, value=60,
-            marks={10: '10', 100: '100'}),
-        html.Br(),
-        dbc.Checklist(
-            options=[{"label": "Capping", "value": 'capping'}],
-            value=[],
-            id="capping-checklist",
-            switch=True,
+controls = dbc.Card(
+    [
+        dbc.CardHeader("Controls"),
+        dbc.CardBody(
+            [
+                html.P("Resolution:"),
+                dcc.Slider(
+                    id="slider-resolution",
+                    min=10,
+                    max=100,
+                    step=1,
+                    value=60,
+                    marks={10: "10", 100: "100"},
+                ),
+                html.Br(),
+                dbc.Checklist(
+                    options=[{"label": "Capping", "value": "capping"}],
+                    value=[],
+                    id="capping-checklist",
+                    switch=True,
+                ),
+            ]
         ),
-    ])
-])
+    ]
+)
 
-app.layout = dbc.Container(fluid=True, children=[
-    html.H1("Demo of dash_vtk.Algorithm"),
-    html.Hr(),
-    dbc.Row([
-        dbc.Col(width=4, children=controls),
-        dbc.Col(width=8, children=[
-            html.Div(
-                vtk_view,
-                style={'height': 'calc(80vh - 20px)', 'width': '100%'}
-            )
-        ])
-    ]),
-])
+app.layout = dbc.Container(
+    fluid=True,
+    children=[
+        html.H1("Demo of dash_vtk.Algorithm"),
+        html.Hr(),
+        dbc.Row(
+            [
+                dbc.Col(width=4, children=controls),
+                dbc.Col(
+                    width=8,
+                    children=[
+                        html.Div(
+                            vtk_view,
+                            style={"height": "calc(80vh - 20px)", "width": "100%"},
+                        )
+                    ],
+                ),
+            ]
+        ),
+    ],
+)
 
 
 @app.callback(
-    Output('vtk-algorithm', 'state'),
-    [Input('slider-resolution', 'value'),
-     Input('capping-checklist', 'value')]
+    Output("vtk-algorithm", "state"),
+    [Input("slider-resolution", "value"), Input("capping-checklist", "value")],
 )
 def update_cone(slider_val, checked_values):
-    return {
-        'resolution': slider_val, 
-        'capping': 'capping' in checked_values
-    }
+    return {"resolution": slider_val, "capping": "capping" in checked_values}
 
 
 if __name__ == "__main__":
