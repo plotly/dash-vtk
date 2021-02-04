03329aa49731ebcef54df7d2c72937422f8dd67a
xhlulu <xhlperso@gmail.com>
2021-02-04 17:26:17 -0500

    npm run build

--- src/DashVtk.jl
+++ src/DashVtk.jl
@@ -3,7 +3,7 @@ module DashVtk
 using Dash
 
 const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
-const version = "0.0.2"
+const version = "0.0.3"
 
 include("vtk_algorithm.jl")
 include("vtk_calculator.jl")
@@ -34,14 +34,14 @@ function __init__()
             [
                 DashBase.Resource(
     relative_package_path = "dash_vtk.min.js",
-    external_url = "https://unpkg.com/dash_vtk@0.0.2/dash_vtk/dash_vtk.min.js",
+    external_url = "https://unpkg.com/dash_vtk@0.0.3/dash_vtk/dash_vtk.min.js",
     dynamic = nothing,
     async = nothing,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "dash_vtk.min.js.map",
-    external_url = "https://unpkg.com/dash_vtk@0.0.2/dash_vtk/dash_vtk.min.js.map",
+    external_url = "https://unpkg.com/dash_vtk@0.0.3/dash_vtk/dash_vtk.min.js.map",
     dynamic = true,
     async = nothing,
     type = :js
