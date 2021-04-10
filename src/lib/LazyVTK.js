13ba560c2f07f22c0ed948c0065bed762ee7db4e
xhlulu <xinghan@plot.ly>
2021-04-10 00:13:07 -0400

    try something that won't work

--- src/lib/LazyVTK.js
+++ src/lib/LazyVTK.js
@@ -1,5 +1,12 @@
-const AsyncView = () => import('react-vtk-js/dist/esm/core/View');
-// const AsyncView = () => import('react-vtk-js').then(ReactVTK => ReactVTK.Core.View);
+// const AsyncView = () => import('react-vtk-js/dist/esm/core/View');
+// const AsyncView = () => import('react-vtk-js').then(ReactVTK => ReactVTK.View);
+
+const AsyncView = async () => {
+    const LazyVTK = await import('react-vtk-js');
+    console.log("LazyVTK", LazyVTK);
+    console.log("LazyVTK.View", LazyVTK.View);
+    return LazyVTK.View;
+}
 
 export {AsyncView};
 export default () => import('react-vtk-js');
