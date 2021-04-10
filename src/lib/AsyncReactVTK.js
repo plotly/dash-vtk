b13ffbb6545266e385265d84f2db0087d48a4cb9
xhlulu <xhlperso@gmail.com>
2021-04-10 01:46:07 -0400

    Add usage simple, simplify import structure

--- src/lib/AsyncReactVTK.js
+++ src/lib/AsyncReactVTK.js
@@ -1,7 +1,7 @@
-import asyncReactVTK from './asyncReactVTK';
+const AsyncReactVTK = import(/*webpackChunkName: "ReactVTK" */ './ReactVTK');
 
 const AsyncView = async () => {
-    const LazyVTK = await asyncReactVTK;
+    const LazyVTK = await AsyncReactVTK;
     console.log("LazyVTK", LazyVTK);
     window.LazyVTK = LazyVTK;
     return {default: LazyVTK.default.View};
--- src/lib/components/View.react.js
+++ src/lib/components/View.react.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 // import { View as VtkItem } from 'react-vtk-js';
-import {AsyncView} from '../LazyVTK';
+import {AsyncView} from '../AsyncReactVTK';
 
 const LazyView = React.lazy(AsyncView);
 
