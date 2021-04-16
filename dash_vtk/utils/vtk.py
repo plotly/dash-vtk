8b2a7aff67b48dd2e23d4994fade3140614e7f96
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2021-04-16 11:00:06 -0600

    fix numpy int64/uint64 conversion

--- dash_vtk/utils/vtk.py
+++ dash_vtk/utils/vtk.py
@@ -1,5 +1,6 @@
 import dash_vtk
 import base64
+import numpy as np
 
 try:
     # v9 and above
@@ -29,6 +30,9 @@ to_js_type = {
 def b64_encode_numpy(obj):
     # Convert 1D numpy arrays with numeric types to memoryviews with
     # datatype and shape metadata.
+    if len(obj) == 0:
+        return obj.tolist()
+
     dtype = obj.dtype
     if (
         dtype.kind in ["u", "i", "f"]
@@ -40,8 +44,31 @@ def b64_encode_numpy(obj):
         buffer = base64.b64encode(memoryview(obj.ravel(order="C"))).decode("utf-8")
         return {"bvals": buffer, "dtype": str(dtype), "shape": obj.shape}
     else:
-        # Convert all other numpy arrays to lists
-        return obj.tolist()
+        buffer = None
+        dtype_str = None
+        # Try to see if we can downsize the array
+        max_value = np.amax(obj)
+        min_value = np.amin(obj)
+        signed = min_value < 0
+        test_value = max(max_value, -min_value)
+        if test_value < np.iinfo(np.int16).max and signed:
+            dtype_str = 'int16'
+            buffer = base64.b64encode(memoryview(obj.astype(np.int16).ravel(order="C"))).decode("utf-8")
+        elif test_value < np.iinfo(np.int32).max and signed:
+            dtype_str = 'int32'
+            buffer = base64.b64encode(memoryview(obj.astype(np.int32).ravel(order="C"))).decode("utf-8")
+        elif test_value < np.iinfo(np.uint16).max and not signed:
+            dtype_str = 'uint16'
+            buffer = base64.b64encode(memoryview(obj.astype(np.uint16).ravel(order="C"))).decode("utf-8")
+        elif test_value < np.iinfo(np.uint32).max and not signed:
+            dtype_str = 'uint32'
+            buffer = base64.b64encode(memoryview(obj.astype(np.uint32).ravel(order="C"))).decode("utf-8")
+
+        if dtype:
+            return {"bvals": buffer, "dtype": dtype_str, "shape": obj.shape}
+
+    # Convert all other numpy arrays to lists
+    return obj.tolist()
 
 
 def to_mesh_state(dataset, field_to_keep=None, point_arrays=None, cell_arrays=None):
